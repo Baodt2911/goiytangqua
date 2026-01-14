394d7fc8067bc7be9869d7b4b2b99ca05a55393c
Repo Committer <devnull@local>
2026-01-14 12:09:57 +0700

    Fix connect socket

--- client/user/src/contexts/socket/socket.provider.tsx
+++ client/user/src/contexts/socket/socket.provider.tsx
@@ -3,14 +3,20 @@ import {
   connectSocket,
   disconnectSocket,
 } from "../../features/socket/socket.service";
+import { useAppSelector } from "../../app/hook";
+import { RootState } from "../../app/store";
 
 export const SocketProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
+  const isAuthenticated = useAppSelector(
+    (state: RootState) => state.auth.isAuthenticated
+  );
   useEffect(() => {
+    if (!isAuthenticated) return;
     connectSocket();
     return () => disconnectSocket();
-  }, []);
+  }, [isAuthenticated]);
 
   return <>{children}</>;
 };
